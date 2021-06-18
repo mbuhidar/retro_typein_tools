6f12ff69d5571a0b052d45b9e2e218db4c0d461d
Michael Buhidar <mbuhidar@gmail.com>
2021-06-18 00:12:43 -0400

    add pytest tests for argparse functionality

---
+++ typeintokenizer/tests/tokenizer_test.py
@@ -0,0 +1,29 @@
+from typeintokenizer.tokenizer import main
+# from typeintokenizer import tokenizer
+import pytest
+
+def test_main_argparse1(capsys):
+    main(['infile.bas', 'outfile.prg'])
+    out_raw, err = capsys.readouterr()
+    out = out_raw.split("\n")[:-1]
+    assert out == ['0x0801', '2', 'ahoy', 'infile.bas', 'outfile.prg']
+    assert err == ''
+
+def test_main_argparse2(capsys):
+    main(['-s', 'pet', 'infile.bas', 'outfile.prg'])
+    out_raw, err = capsys.readouterr()
+    out = out_raw.split("\n")[:-1]
+    assert out == ['0x0801', '2', 'pet', 'infile.bas', 'outfile.prg']
+    assert err == ''
+
+def test_main_argparse3(capsys):
+    main(['-v', '7', 'infile.bas', 'outfile.prg'])
+    out_raw, err = capsys.readouterr()
+    out = out_raw.split("\n")[:-1]
+    assert out == ['0x0801', '7', 'ahoy', 'infile.bas', 'outfile.prg']
+
+def test_main_argparse4(capsys):
+    main(['-l', '0x1001', 'infile.bas', 'outfile.prg'])
+    out_raw, err = capsys.readouterr()
+    out = out_raw.split("\n")[:-1]
+    assert out == ['0x1001', '2', 'ahoy', 'infile.bas', 'outfile.prg']
--- typeintokenizer/tokenizer.py
+++ typeintokenizer/tokenizer.py
@@ -4,13 +4,13 @@ import argparse
 from argparse import RawTextHelpFormatter
 import sys
 
-def main():
+def main(argv=None):
     # create parser object
     parser = argparse.ArgumentParser(description = "A tokenizer for Commodore BASIC typein programs.", formatter_class=RawTextHelpFormatter)
 
     # define arguments for parser object
     parser.add_argument("-l", "--loadaddr", type=str, nargs=1, required=False,
-                        metavar = "load_address", default = "0x0801",
+                        metavar = "load_address", default = ["0x0801"],
                         help = "Specifies the target BASIC memory address when loading:\n"
                                 "- 0x0801 - C64 (default)\n"
                                 "- 0x1001 - VIC20 Unexpanded\n"
@@ -19,8 +19,8 @@ def main():
                                 "- 0x1201 - VIC20 +16\n"
                                 "- 0x1201 - VIC20 +24K")
 
-    parser.add_argument("-v", "--version", choices=[1, 2, 3, 4, 7], type=int, nargs=1, required=False,
-                        metavar = "basic_version", default=2,
+    parser.add_argument("-v", "--version", choices=['1', '2', '3', '4', '7'], type=str, nargs=1, required=False,
+                        metavar = "basic_version", default=['2'],
                         help = "Specifies the BASIC version for use in tokenizing file.\n"
                         "- 1 - Basic v1.0  PET\n"
                         "- 2 - Basic v2.0  C64/VIC20/PET (default)\n"
@@ -29,7 +29,7 @@ def main():
                         "- 7 - Basic v7.0  C128")
 
     parser.add_argument("-s", "--source", choices=["pet", "ahoy"], type=str, nargs=1, required=False,
-                        metavar = "source_format", default="ahoy",
+                        metavar = "source_format", default=["ahoy"],
                         help = "Specifies the source BASIC file format:\n"
                         "pet - use standard pet control character mnemonics\n"
                         "ahoy - use Ahoy! magazine control character mnemonics (default)")
@@ -41,14 +41,15 @@ def main():
                         help = "Specify the output file name including path")
                         
     # parse the arguments from standard input
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     # test args capture
-    print(args.loadaddr)
-    print(args.version)
-    print(args.source)
+    print(args.loadaddr[0])
+    print(args.version[0])
+    print(args.source[0])
     print(args.file_in)
     print(args.file_out)
     
 if __name__ == '__main__':
     sys.exit(main())
+
