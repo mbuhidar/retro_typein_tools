6d4dc3938b3cc5fd1a566e7af983edb9cf714dba
Michael Buhidar <mbuhidar@gmail.com>
2021-06-19 16:53:58 -0400

    Refactor test for parse_args function

--- typein_tokenizer/tests/tokenizer_test.py
+++ typein_tokenizer/tests/tokenizer_test.py
@@ -1,29 +1,25 @@
 from typein_tokenizer.tokenizer import parse_args
 import pytest
 
-def test_parse_args1():
-    argv = ['infile.bas', 'outfile.prg']
-    args = parse_args(argv)
-    arg_list = [args.loadaddr[0], args.version[0], args.source[0], args.file_in, args.file_out]
-    assert arg_list == ['0x0801', '2', 'ahoy', 'infile.bas', 'outfile.prg']
-'''
-def test_main_argparse2(capsys):
-    main(['-s', 'pet', 'infile.bas', 'outfile.prg'])
-    out_raw, err = capsys.readouterr()
-    out = out_raw.split("\n")[:-1]
-    assert out == ['0x0801', '2', 'pet', 'infile.bas', 'outfile.prg']
-    assert err == ''
-
-def test_main_argparse3(capsys):
-    main(['-v', '7', 'infile.bas', 'outfile.prg'])
-    out_raw, err = capsys.readouterr()
-    out = out_raw.split("\n")[:-1]
-    assert out == ['0x0801', '7', 'ahoy', 'infile.bas', 'outfile.prg']
-
-def test_main_argparse4(capsys):
-    main(['-l', '0x1001', 'infile.bas', 'outfile.prg'])
-    out_raw, err = capsys.readouterr()
-    out = out_raw.split("\n")[:-1]
-    assert out == ['0x1001', '2', 'ahoy', 'infile.bas', 'outfile.prg']
-'''
+def test_parse_args():
+    argv = [
+            ['infile.bas', 'outfile.prg'],
+            ['infile.bas', 'outfile.prg', '-s', 'pet'],
+            ['infile.bas', 'outfile.prg', '-v', '7'],
+            ['infile.bas', 'outfile.prg', '-l', '0x1001'],
+            ['-v', '4', 'infile.bas', 'outfile.prg', '-s', 'ahoy', '-l', '0x1001']
+           ]
+            
+    arg_valid = [
+                 ['0x0801', '2', 'ahoy', 'infile.bas', 'outfile.prg'],
+                 ['0x0801', '2', 'pet', 'infile.bas', 'outfile.prg'],
+                 ['0x0801', '7', 'ahoy', 'infile.bas', 'outfile.prg'],
+                 ['0x1001', '2', 'ahoy', 'infile.bas', 'outfile.prg'],
+                 ['0x1001', '4', 'ahoy', 'infile.bas', 'outfile.prg']
+                ]
+  
+    for i in range(len(argv)):
+        args = parse_args(argv[i])
+        arg_list = [args.loadaddr[0], args.version[0], args.source[0], args.file_in, args.file_out]
+        assert arg_list == arg_valid[i]
 
