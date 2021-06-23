a5bd6cd3cefe639e72048c5f68a3ec8b941c9eee
Michael Buhidar <mbuhidar@gmail.com>
2021-06-23 00:10:52 -0400

    Fix code formatting and add outline for tokenizer feature.

--- typein_tokenizer/tokenizer.py
+++ typein_tokenizer/tokenizer.py
@@ -86,12 +86,15 @@ TOKENS = (
 def parse_args(argv):
 
     # create parser object
-    parser = argparse.ArgumentParser(description = "A tokenizer for Commodore BASIC typein programs.", formatter_class=RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description =\
+    "A tokenizer for Commodore BASIC typein programs.",\
+    formatter_class=RawTextHelpFormatter)
 
     # define arguments for parser object
     parser.add_argument("-l", "--loadaddr", type=str, nargs=1, required=False,
                         metavar = "load_address", default = ["0x0801"],
-                        help = "Specifies the target BASIC memory address when loading:\n"
+                        help = "Specifies the target BASIC memory address \
+                                when loading:\n"
                                 "- 0x0801 - C64 (default)\n"
                                 "- 0x1001 - VIC20 Unexpanded\n"
                                 "- 0x0401 - VIC20 +3K\n"
@@ -99,20 +102,24 @@ def parse_args(argv):
                                 "- 0x1201 - VIC20 +16\n"
                                 "- 0x1201 - VIC20 +24K")
 
-    parser.add_argument("-v", "--version", choices=['1', '2', '3', '4', '7'], type=str, nargs=1, required=False,
+    parser.add_argument("-v", "--version", choices=['1', '2', '3', '4', '7'],
+                        type=str, nargs=1, required=False,
                         metavar = "basic_version", default=['2'],
-                        help = "Specifies the BASIC version for use in tokenizing file.\n"
+                        help = "Specifies the BASIC version for use in \
+                                tokenizing file.\n"
                         "- 1 - Basic v1.0  PET\n"
                         "- 2 - Basic v2.0  C64/VIC20/PET (default)\n"
                         "- 3 - Basic v3.5  C16/C116/Plus/4\n"
                         "- 4 - Basic v4.0  PET/CBM2\n"
                         "- 7 - Basic v7.0  C128")
 
-    parser.add_argument("-s", "--source", choices=["pet", "ahoy"], type=str, nargs=1, required=False,
+    parser.add_argument("-s", "--source", choices=["pet", "ahoy"], type=str,
+                        nargs=1, required=False,
                         metavar = "source_format", default=["ahoy"],
                         help = "Specifies the source BASIC file format:\n"
                         "pet - use standard pet control character mnemonics\n"
-                        "ahoy - use Ahoy! magazine control character mnemonics (default)")
+                        "ahoy - use Ahoy! magazine control character mnemonics \
+                        (default)")
 
     parser.add_argument("file_in", type=str, metavar="input_file",
                         help = "Specify the input file name including path")
@@ -148,6 +155,12 @@ class TokenizedLine():
         return len(self.bytes) = 5
 '''
         
+# split each line into line number and remaining line
+# convert unique magazine character codes to petcat character codes (preserve)
+# tokenize remaining line
+# - don't tokenize words within print quotes or in REM statements
+# - 
+
 def main(argv=None):
     # call function to parse command line input arguments
     args = parse_args(argv)
@@ -166,9 +179,9 @@ def main(argv=None):
     # call function to read input file lines and print each line
     lines_list = read_file(args.file_in)
     for line in lines_list:
-        tokenized_line = TokenizedLine(line, addr)
-        addr += len(tokenized_line)
-        tokenized_lines.append(tokenized_line)
+        # tokenized_line = TokenizedLine(line, addr)
+        # addr += len(tokenized_line)
+        # tokenized_lines.append(tokenized_line)
         print(line)
 
 if __name__ == '__main__':
